c3474cdb8024da4b32564e683787ed198f97e669
abdulloooh <abdullahakinwumi@gmail.com>
2020-09-01 01:21:18 +0100

    optimized summary logic

--- src/services/fakeIndividualList.js
+++ src/services/fakeIndividualList.js
@@ -8,57 +8,54 @@ import { getDebts } from "./fakeDebtList";
 
 export function getIndividualDebts() {
   let debts = getDebts();
-  var { uniqueNameList } = getNameList(debts);
+  let individualList = [];
+  let finalBalance = 0;
 
-  return classifyIndividualDebts(uniqueNameList, debts);
-}
+  debts.forEach(function (debt) {
+    //Simply add credittotal and debittotal
+    if (debt.name === "Total")
+      finalBalance =
+        finalBalance + (debt.status === "cr" ? +debt.amount : -debt.amount);
 
-function classifyIndividualDebts(uniqueNameList, debts) {
-  let total = { name: "Total Balance" };
-  let totalAmount = 0;
-  let totalOwedToMe = 0;
-  let totalOwedByMe = 0;
+    if (individualList.length === 0)
+      if (debt.name !== "Total")
+        return individualList.push(formatIndividual(debt)); //start off by pushing sth in the equivalent array
 
-  let individual = [];
-  uniqueNameList.forEach((u) => {
-    let debtsPerNameList = debts.filter((d) => d.name === u);
-    let debitBalance = 0;
-    let creditBalance = 0;
-    debtsPerNameList.forEach((d) => {
-      if (d.status === "cr") creditBalance += Number(d.amount);
-      else if (d.status === "dr") debitBalance += Number(d.amount);
-    });
-    let newItem = {}; //create new property with empty object value
-    newItem.name = u;
-    newItem.tome = creditBalance;
-    newItem.byme = debitBalance;
-    newItem.balance = Number(creditBalance) - Number(debitBalance);
-    if (newItem.name !== "Total") individual.push(newItem); //Do not push total credit and debit balance
+    const sameNameIndex = individualList.findIndex(
+      (item) => item.name === debt.name
+    ); //check if matching name exist
 
-    if (newItem.name === "Total") {
-      //Calculate total balance only from total credit and debit balance
-      totalOwedByMe = Number(totalOwedByMe) + Number(newItem.byme);
-      totalOwedToMe = Number(totalOwedToMe) + Number(newItem.tome);
-      totalAmount = Number(totalAmount) + Number(newItem.balance);
+    //if not found already
+    if (sameNameIndex === -1) {
+      if (debt.name !== "Total") {
+        //ignore debit and credit total
+        individualList.push(formatIndividual(debt));
+      }
+    } else {
+      const formattedDebt = formatIndividual(debt);
+      const thisDebtBalance = formattedDebt.tome - formattedDebt.byme;
+      const data = individualList[sameNameIndex];
+      data.tome = Number(data.tome) + Number(formattedDebt.tome);
+      data.byme = Number(data.byme) + Number(formattedDebt.byme);
+      data.balance = Number(data.balance) + thisDebtBalance;
     }
   });
 
-  total.balance = totalAmount;
-  // total.tome = totalOwedToMe;
-  // total.byme = totalOwedByMe;
-  total.common = "total";
-  individual.push(total);
-
-  return individual;
+  individualList.push({
+    name: "Total Balance",
+    balance: finalBalance,
+    common: "total",
+  });
+  return individualList;
 }
 
-function getNameList(debts) {
-  let nameList = [];
-  debts.forEach((d) => {
-    d.name && nameList.push(d.name);
-  });
-  let uniqueNameList = nameList.filter(
-    (n, index) => nameList.indexOf(n) === index
-  );
-  return { uniqueNameList };
+function formatIndividual(item) {
+  let { name, status, amount } = item;
+  amount = Number(amount); //double sure
+  return {
+    tome: status === "cr" ? amount : 0,
+    byme: status === "dr" ? amount : 0,
+    balance: status === "cr" ? +amount : -amount,
+    name: name,
+  };
 }
