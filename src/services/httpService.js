098b7788ad020709a7457942d214c44b3d5fe898
abdulloooh <abdullahakinwumi@gmail.com>
2020-09-20 12:29:05 +0100

    debugging cookie et

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -21,6 +21,8 @@ axios.interceptors.response.use(null, (error) => {
   if (!expectedError) {
     toast.error("Ouch! Unexpected error, please log in again");
     localStorage.removeItem("username");
+    console.log(error);
+    return false;
     logger.log(error);
     window.location = "/login";
   }
