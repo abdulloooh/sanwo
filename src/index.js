b489010e16fee8e3cacd1bd330ef2164a6f59b69
Abdullah Oladipo <abdullahakinwumi@gmail.com>
2025-02-22 00:01:07 +0100

    Add functionality to install the PWA from the website

--- src/index.js
+++ src/index.js
@@ -7,6 +7,39 @@ import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.scss";
 
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+});
+
+function showInstallPromotion() {
+  const installButton = document.getElementById('installButton');
+  installButton.style.display = 'block';
+
+  installButton.addEventListener('click', async () => {
+    // Hide the app provided install promotion
+    installButton.style.display = 'none';
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    const { outcome } = await deferredPrompt.userChoice;
+    // Optionally, send analytics event with outcome of user choice
+    console.log(`User response to the install prompt: ${outcome}`);
+    // We've used the prompt, and can't use it again, throw it away
+    deferredPrompt = null;
+  });
+}
+
+window.addEventListener('appinstalled', () => {
+  console.log('PWA was installed');
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
