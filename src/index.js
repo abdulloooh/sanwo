d9a3ec8a93c1046a75f96168f88df5e2981f5b21
Abdullah Oladipo <abdullahakinwumi@gmail.com>
2025-02-22 01:08:35 +0100

    Remove commented out code for beforeinstallprompt event

--- src/index.js
+++ src/index.js
@@ -7,17 +7,15 @@ import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.scss";
 
-// window.addEventListener('beforeinstallprompt', (e) => {
-//   // Prevent the mini-infobar from appearing on mobile
-//   e.preventDefault();
-//   // Stash the event so it can be triggered later.
-//   deferredPrompt = e;
-//   // Update UI notify the user they can install the PWA
-//   showInstallPromotion();
-// });
-
 window.addEventListener('appinstalled', () => {
   console.log('PWA was installed');
+});
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-info bar from appearing on mobile
+  e.preventDefault();
+  // Show the install prompt
+  window.showInstallPrompt();
 });
 
 ReactDOM.render(
