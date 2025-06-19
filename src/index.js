6c698a944aa0f884e7cb906cb92589947f93b1cd
Abdullah Oladipo <abdullahakinwumi@gmail.com>
2025-06-19 08:10:20 +0100

    fixed service worker for PWA

--- src/index.js
+++ src/index.js
@@ -7,17 +7,6 @@ import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.scss";
 
-window.addEventListener('appinstalled', () => {
-  console.log('PWA was installed');
-});
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-info bar from appearing on mobile
-  e.preventDefault();
-  // Show the install prompt
-  window.showInstallPrompt();
-});
-
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
